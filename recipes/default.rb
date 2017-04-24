a0d43c4cf73f7b6ba327ec7b2be770408ca2b4e5
Matt <email@example.com>
2017-04-24 18:15:19 +0000

    adding solution files

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,40 @@
 # Recipe:: default
 #
 # Copyright:: 2017, Student Name, All Rights Reserved.
+
+include_recipe 'php::default'
+
+package 'net-tools'
+
+package 'apache package' do
+  case node[:platform]
+  when 'centos'
+    package_name 'httpd'
+  when 'ubuntu'
+    package_name 'apache2'
+  end
+end
+
+service 'apache service' do
+  case node[:platform]
+  when 'centos'
+    service_name 'httpd' 
+  when 'ubuntu'
+    service_name 'apache2' 
+  end
+    action [:enable, :start]
+end
+
+group 'developers'
+
+user 'webadmin' do
+  manage_home true
+  gid   'developers'
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  owner 'root'
+  group 'developers'
+  mode '664'
+end
